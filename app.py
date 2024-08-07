f505eb985cd9020207a7fafab048e4d7f6dd9d1e
Yeonsu Kim <85694785+Yeoonsu@users.noreply.github.com>
2024-08-07 19:16:13 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+# Streamlit 제목
+st.title('CSV VIEWER')
+
+# 파일 업로드
+uploaded_file = st.file_uploader("Upload CSV file", type="csv")
+
+if uploaded_file is not None:
+    # CSV 파일을 데이터프레임으로 읽기
+    df = pd.read_csv(uploaded_file)
+    
+    # 데이터프레임 출력
+    st.write(df)
